4b7b46e3eaea38b7b87edd55939aaf2572f7d1b9
chankruze <chandu.chandaniit@gmail.com>
2018-11-09 15:31:23 +0000

    Updated Build Script

--- compilation/Build-Vanilla-840N-v2.sh
+++ compilation/Build-Vanilla-840N-v2.sh
@@ -8,12 +8,26 @@ echo " --------------------------------------------------"
 echo "             https://github.com/chankruze          "
 echo " --------------------------------------------------"
 echo " "
+echo "Preparing Resources ..........."
 git clone https://www.github.com/openwrt/openwrt -b openwrt-18.06
 cd openwrt
+echo " "
+echo "Updating Sources .............."
+echo " "
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+echo " "
+echo "Configuring Build ............."
+echo " "
 wget https://raw.githubusercontent.com/chankruze/TL-WR840N/configs/TL_WR840N_v2_defconfig
-make TL_WR840N_v2_defconfig
+rm .config
+mv TL_WR840N_v2_defconfig .config
 make defconfig
+echo " "
+echo "Downloading Latest Packages ............"
+echo " "
 make download
+echo " "
+echo "Compilation Started ............."
+echo " "
 make -j4
